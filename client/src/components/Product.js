81b207182ebc123aaaa1b1302f177a5f5a3dc511
Antu_Das <antuchandradasp9@gmail.com>
2023-05-02 09:02:02 +0600

    Made some changes

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-const Product = () => {
-  return <div>Product</div>;
+
+const Product = ({ product }) => {
+
+  // console.log()
+
+
+  return (
+    <Link>
+      <div>
+        {/* Images  */}
+        <div className='w-full h-[200px] flex items-center justify-center relative'>
+          <img
+            className='w-[160px] h-[160px] group-hover:scale-90'
+            src={`http://localhost:1337${product.attributes.image.data[0].attributes.url}`} alt="" />
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Product;
--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -1,7 +1,33 @@
 import React from 'react';
+// Importing React Swiper Component 
+import { Swiper, SwiperSlide } from "swiper/react";
+// Importing Swiper Styles 
+import "swiper/css";
+import "swiper/css/pagination";
+// Importing Required Modules 
+import { Pagination, Navigation } from "swiper";
+import Product from "../components/Product";
 
-const ProductSlider = () => {
-  return <div>ProductSlider</div>;
+
+const ProductSlider = ({ data }) => {
+
+  return (
+    <Swiper>
+      <>
+        {
+          data?.map((product) => {
+            return <SwiperSlide
+              key={product.id}
+            >
+              <Product
+                product={product}
+              />
+            </SwiperSlide>
+          })
+        }
+      </>
+    </Swiper>
+  );
 };
 
 export default ProductSlider;
